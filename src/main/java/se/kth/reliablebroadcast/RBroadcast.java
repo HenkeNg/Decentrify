b5907e9efab96d2714421588b8db79b3e846a0ee
Mikael Mölder <mikael@n156-p20.kthopen.kth.se>
2017-03-31 10:43:16 +0200

    fixed rb deliver and broadcast events

--- src/main/java/se/kth/reliablebroadcast/RBroadcast.java
+++ src/main/java/se/kth/reliablebroadcast/RBroadcast.java
@@ -6,4 +6,14 @@ import se.sics.kompics.KompicsEvent;
  * Created by mikael on 2017-03-31.
  */
 public class RBroadcast implements KompicsEvent {
+
+    private Object payload;
+
+    public RBroadcast(Object payload) {
+        this.payload = payload;
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
 }
--- src/main/java/se/kth/reliablebroadcast/RDeliver.java
+++ src/main/java/se/kth/reliablebroadcast/RDeliver.java
@@ -1,9 +1,25 @@
 package se.kth.reliablebroadcast;
 
 import se.sics.kompics.KompicsEvent;
+import se.sics.ktoolbox.util.network.KAddress;
 
 /**
  * Created by mikael on 2017-03-31.
  */
 public class RDeliver implements KompicsEvent {
+    private KAddress source;
+    private Object payload;
+
+    public RDeliver(KAddress source, Object payload) {
+        this.source = source;
+        this.payload = payload;
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
+
+    public KAddress getSource() {
+        return source;
+    }
 }
